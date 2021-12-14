2a8b3396530f75066788413dcbdf7939275bdea0
waldofx <90760767+waldofx@users.noreply.github.com>
2021-12-14 17:58:25 +0700

    About Us Logos Complete

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,6 +1,14 @@
 import React from "react";
 import styles from "./AboutUs.module.css";
 
+//import svgs
+import Golang from "../assets/img/Golang.svg";
+import Javascript from "../assets/img/Javascript.svg";
+import Html from "../assets/img/Html.svg";
+import Bootstrap from "../assets/img/Bootstrap.svg";
+import Css from "../assets/img/Css.svg";
+import ReactSVG from "../assets/img/React.svg";
+
 function AboutUs() {
     return (
         <div>
@@ -19,6 +27,14 @@ function AboutUs() {
             </div>
             <div>
                 <p className={styles.built}>BUILT WITH</p>
+                <div className={styles.logos}>
+                    <img src={Golang} alt="Golang.sgv" />
+                    <img src={Javascript} alt="Javascript.sgv" />
+                    <img src={Html} alt="Html.sgv" />
+                    <img src={Bootstrap} alt="Bootstrap.sgv" />
+                    <img src={Css} alt="Css.sgv" />
+                    <img src={ReactSVG} alt="React.sgv" />
+                </div>
             </div>
         </div>
     );
