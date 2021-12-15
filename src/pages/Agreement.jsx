80e25dc5993c59b8e61bcfc6d3cc8858efc6aa3e
waldofx <90760767+waldofx@users.noreply.github.com>
2021-12-15 17:49:03 +0700

    Agreement complete

--- src/pages/Agreement.jsx
+++ src/pages/Agreement.jsx
@@ -11,111 +11,135 @@ function Agreement() {
             <Header />
             <div>
                 <h1>Terms and Agreements</h1>
-                <p className={styles.desc}>
-                    This website is operated by COMPANY NAME. Throughout the
-                    site, the terms “we”, “us” and “our” refer to COMPANY NAME.
-                    COMPANY NAME offers this website, including all information,
-                    tools and services available from this site to you, the
-                    user, conditioned upon your acceptance of all terms,
-                    conditions, policies and notices stated here. By visiting
-                    our site and/ or purchasing something from us, you engage in
-                    our “Service” and agree to be bound by the following terms
-                    and conditions (“Terms of Service”, “Terms”), including
-                    those additional terms and conditions and policies
-                    referenced herein and/or available by hyperlink. These Terms
-                    of Service apply to all users of the site, including without
-                    limitation users who are browsers, vendors, customers,
-                    merchants, and/ or contributors of content. Please read
-                    these Terms of Service carefully before accessing or using
-                    our website. By accessing or using any part of the site, you
-                    agree to be bound by these Terms of Service. If you do not
-                    agree to all the terms and conditions of this agreement,
-                    then you may not access the website or use any services. If
-                    these Terms of Service are considered an offer, acceptance
-                    is expressly limited to these Terms of Service. Any new
-                    features or tools which are added to the current store shall
-                    also be subject to the Terms of Service. You can review the
-                    most current version of the Terms of Service at any time on
-                    this page. We reserve the right to update, change or replace
-                    any part of these Terms of Service by posting updates and/or
-                    changes to our website. It is your responsibility to check
-                    this page periodically for changes. Your continued use of or
-                    access to the website following the posting of any changes
-                    constitutes acceptance of those changes. Our store is hosted
-                    on ECOMMERCE PLATFORM. They provide us with the online
-                    e-commerce platform that allows us to sell our products and
-                    services to you. SECTION 1 – ONLINE STORE TERMS By agreeing
-                    to these Terms of Service, you represent that you are at
-                    least the age of majority in your state or province of
-                    residence, or that you are the age of majority in your state
-                    or province of residence and you have given us your consent
-                    to allow any of your minor dependents to use this site. You
-                    may not use our products for any illegal or unauthorized
-                    purpose nor may you, in the use of the Service, violate any
-                    laws in your jurisdiction (including but not limited to
-                    copyright laws). You must not transmit any worms or viruses
-                    or any code of a destructive nature. A breach or violation
-                    of any of the Terms will result in an immediate termination
-                    of your Services. SECTION 2 – GENERAL CONDITIONS We reserve
-                    the right to refuse service to anyone for any reason at any
-                    time. You understand that your content (not including credit
-                    card information), may be transferred unencrypted and
-                    involve (a) transmissions over various networks; and (b)
-                    changes to conform and adapt to technical requirements of
-                    connecting networks or devices. Credit card information is
-                    always encrypted during transfer over networks. You agree
-                    not to reproduce, duplicate, copy, sell, resell or exploit
-                    any portion of the Service, use of the Service, or access to
-                    the Service or any contact on the website through which the
-                    service is provided, without express written permission by
-                    us. The headings used in this agreement are included for
-                    convenience only and will not limit or otherwise affect
-                    these Terms. SECTION 3 – ACCURACY, COMPLETENESS AND
-                    TIMELINESS OF INFORMATION We are not responsible if
-                    information made available on this site is not accurate,
-                    complete or current. The material on this site is provided
-                    for general information only and should not be relied upon
-                    or used as the sole basis for making decisions without
-                    consulting primary, more accurate, more complete or more
-                    timely sources of information. Any reliance on the material
-                    on this site is at your own risk. This site may contain
-                    certain historical information. Historical information,
-                    necessarily, is not current and is provided for your
-                    reference only. We reserve the right to modify the contents
-                    of this site at any time, but we have no obligation to
-                    update any information on our site. You agree that it is
-                    your responsibility to monitor changes to our site. SECTION
-                    4 – MODIFICATIONS TO THE SERVICE AND PRICES Prices for our
-                    products are subject to change without notice. We reserve
-                    the right at any time to modify or discontinue the Service
-                    (or any part or content thereof) without notice at any time.
-                    We shall not be liable to you or to any third-party for any
-                    modification, price change, suspension or discontinuance of
-                    the Service. SECTION 5 – PRODUCTS OR SERVICES (if
-                    applicable) Certain products or services may be available
-                    exclusively online through the website. These products or
-                    services may have limited quantities and are subject to
-                    return or exchange only according to our Return Policy. We
-                    have made every effort to display as accurately as possible
-                    the colors and images of our products that appear at the
-                    store. We cannot guarantee that your computer monitor’s
-                    display of any color will be accurate. We reserve the right,
-                    but are not obligated, to limit the sales of our products or
-                    Services to any person, geographic region or jurisdiction.
-                    We may exercise this right on a case-by-case basis. We
-                    reserve the right to limit the quantities of any products or
-                    services that we offer. All descriptions of products or
-                    product pricing are subject to change at anytime without
-                    notice, at the sole discretion of us. We reserve the right
-                    to discontinue any product at any time. Any offer for any
-                    product or service made on this site is void where
-                    prohibited. We do not warrant that the quality of any
-                    products, services, information, or other material purchased
-                    or obtained by you will meet your expectations, or that any
-                    errors in the Service will be corrected. Sincidental
-                    damages, in such states or jurisdictions, our liability
-                    shall be limited to the maximum extent permitted by law.
-                </p>
+                <div className={styles.desc}>
+                    <p>
+                        This website is operated by members of Group 9 which
+                        include Restu Fajar Sidhiq, Waldo Felix, and Yesaya
+                        Alehandro Silalahi. Throughout the site, the terms “we”,
+                        “us” and “our” refer to Group 9. Group 9 offers this
+                        website, including all information, tools and services
+                        available from this site to you, the user, conditioned
+                        upon your acceptance of all terms, conditions, policies
+                        and notices stated here. By visiting our site and
+                        browsing the contents, you engage in our “Service” and
+                        agree to be bound by the following terms and conditions
+                        (“Terms of Service”, “Terms”), including those
+                        additional terms and conditions and policies referenced
+                        herein and/or available by hyperlink. These Terms of
+                        Service apply to all users of the site, including
+                        without limitation users who are browsers, vendors,
+                        customers, merchants, and/ or contributors of content.
+                        Please read these Terms of Service carefully before
+                        accessing or using our website. By accessing or using
+                        any part of the site, you agree to be bound by these
+                        Terms of Service. If you do not agree to all the terms
+                        and conditions of this agreement, then you may not
+                        access the website or use any services. If these Terms
+                        of Service are considered an offer, acceptance is
+                        expressly limited to these Terms of Service. Any new
+                        features which are added to the current website shall
+                        also be subject to the Terms of Service. You can review
+                        the most current version of the Terms of Service at any
+                        time on this page. We reserve the right to update,
+                        change or replace any part of these Terms of Service by
+                        posting updates and/or changes to our website. It is
+                        your responsibility to check this page periodically for
+                        changes. Your continued use of or access to the website
+                        following the posting of any changes constitutes
+                        acceptance of those changes.
+                    </p>
+                    <br />
+                    <h6>SECTION 1 - REGISTRATION</h6>
+                    <p>
+                        Before you can start posting and participating in
+                        discussions on INFION, You need to understand and agree
+                        to these terms:
+                        <ol type="a">
+                            <li>
+                                By registering an account, you are willing to
+                                provide us with your e-mail adress, username,
+                                and password.
+                            </li>
+                            <li>
+                                Your username may not imitate another individual
+                                or organisation without any permission.
+                            </li>
+                            <li>
+                                Your username may not imitate another individual
+                                or organisation with the intent to pretend as
+                                that individual or oranisation.
+                            </li>
+                            <li>
+                                Your username may not contain any offensive or
+                                inappropriate words.
+                            </li>
+                            <li>
+                                Your username belongs to INFION, and we hold all
+                                rights to change or remove it.
+                            </li>
+                            <li>
+                                You are responsible for keeping your username
+                                and password a secret.
+                            </li>
+                            <li>
+                                You are responsible for the legitimacy of your
+                                registered datas.
+                            </li>
+                            <li>
+                                We are not responsible for any legal troubles
+                                caused by your registered datas.
+                            </li>
+                        </ol>
+                    </p>
+                    <br />
+                    <h6>SECTION 2 - Content</h6>
+                    <p>
+                        Before you can start posting and participating in
+                        discussions on INFION, You need to understand and agree
+                        to these terms:
+                        <ol type="a">
+                            <li>
+                                All users are solely responsible of all content
+                                they posted in Infion. Infion shall not be held
+                                responsible in any way should the content posted
+                                by the users cause any trouble.
+                            </li>
+                            <li>
+                                We hold the right to give any detail and
+                                information of any content the user posted
+                                towards the law enforcement should the user be
+                                caught in legal trouble.
+                            </li>
+                            <li>
+                                The content you post must not contain any of the
+                                following:
+                                <ol type="i">
+                                    <li>False information.</li>
+                                    <li>Illegal content.</li>
+                                    <li>Copyrighted content.</li>
+                                    <li>
+                                        Information of a third party without
+                                        permission.
+                                    </li>
+                                    <li>
+                                        Discrimination towards any tribe,
+                                        religion, race, group, or gender.
+                                    </li>
+                                    <li>Spam.</li>
+                                    <li>Pornography.</li>
+                                    <li>
+                                        Virus or any other malicious software.
+                                    </li>
+                                </ol>
+                            </li>
+                        </ol>
+                    </p>
+                    <br />
+                    <p>
+                        A breach or violation of any of the Terms will result in
+                        an immediate termination of your account.
+                    </p>
+                </div>
             </div>
             <Footer />
         </div>
