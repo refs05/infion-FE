acc7a323895d185fc841fb16c228285118c281b8
andros3c <alehandro4354@gmail.com>
2021-12-21 13:45:09 +0700

    add loading component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import Loading from "./components/loading/Loading";
+import Ld_page from "./components/main_ldpage_unacc/Ld_page";
 function App() {
   return (
     <div className="App">
-      <Loading></Loading>
+      <Ld_page></Ld_page>
     </div>
   );
 }
