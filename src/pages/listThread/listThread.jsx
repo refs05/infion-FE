84d03932a2f21424610a3b96913954688a01b8ee
Restu Fajar Sidhiq <restufajarsidhiq02@gmail.com>
2022-01-14 14:17:08 +0700

    Fix Filter & Category

--- src/pages/listThread/listThread.jsx
+++ src/pages/listThread/listThread.jsx
@@ -20,6 +20,9 @@ const ListThread = (props) => {
 
     const [loading, setLoading] = useState(true);
     const [data, setData] = useState([]);
+    const [filteredThread, setFilteredThread] = useState([])
+    const [filter, setFilter] = useState("")
+    const [category, setCategory] = useState("")
 
     useEffect(() => {
         const fetchData = async () => {
@@ -36,6 +39,29 @@ const ListThread = (props) => {
         fetchData();
     }, []);
 
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            try {
+              const {data: response} = await axios.get(`http://localhost:8000/threads/list/?category=${category}&sortBy=${filter}`);
+              setFilteredThread(response);
+            } catch (error) {
+                console.error(error.message);
+            }
+            setLoading(false);
+        };
+
+        fetchData();
+    }, [filter, category]);
+    
+    function onChangeFilter(event) {
+        setFilter(event.target.value);
+    }
+
+    function onChangeCategory(event) {
+        setCategory(event.target.value);
+    }
+
     return (
         <>
             <Header />
@@ -46,43 +72,71 @@ const ListThread = (props) => {
                         <div className="title d-flex justify-content-center mb-1">Filter</div>
                         <div className="content">
                             <div className="mb-1 form-check">
-                                <input className="form-check-input me-2" type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked/>
+                                <input className="form-check-input me-2" type="radio" name="flexRadioDefault" id="flexRadioDefault1" 
+                                value=""
+                                checked={filter === ""}
+                                onChange={onChangeFilter}
+                                />
                                 <label className="form-check-label" for="flexRadioDefault1">
                                     None
                                 </label>
                             </div>
                             <div className="mb-1 form-check">
-                                <input className="form-check-input me-2" type="radio" name="flexRadioDefault" id="flexRadioDefault2"/>
+                                <input className="form-check-input me-2" type="radio" name="flexRadioDefault" id="flexRadioDefault2"
+                                value="created_at desc"
+                                checked={filter === "created_at desc"}
+                                onChange={onChangeFilter}
+                                />
                                 <label className="form-check-label" for="flexRadioDefault2">
-                                    Oldestt
+                                    Oldest
                                 </label>
                             </div>
                             <div className="mb-1 form-check">
-                                <input className="form-check-input me-2" type="radio" name="flexRadioDefault" id="flexRadioDefault3" />
+                                <input className="form-check-input me-2" type="radio" name="flexRadioDefault" id="flexRadioDefault3" 
+                                value="created_at asc"
+                                checked={filter === "created_at asc"}
+                                onChange={onChangeFilter}
+                                />
                                 <label className="form-check-label" for="flexRadioDefault3">
                                     Newest
                                 </label>
                             </div>
                             <div className="mb-1 form-check">
-                                <input className="form-check-input me-2" type="radio" name="flexRadioDefault" id="flexRadioDefault4" />
+                                <input className="form-check-input me-2" type="radio" name="flexRadioDefault" id="flexRadioDefault4" 
+                                value="like_count desc"
+                                checked={filter === "like_count desc"}
+                                onChange={onChangeFilter}
+                                />
                                 <label className="form-check-label" for="flexRadioDefault4">
                                     Top Threads
                                 </label>
                             </div>
                             <div className="mb-1 form-check">
-                                <input className="form-check-input me-2" type="radio" name="flexRadioDefault" id="flexRadioDefault5" />
+                                <input className="form-check-input me-2" type="radio" name="flexRadioDefault" id="flexRadioDefault5" 
+                                value="like_count desc"
+                                checked={filter === "like_count desc"}
+                                onChange={onChangeFilter}
+                                />
                                 <label className="form-check-label" for="flexRadioDefault5">
                                     By Like
                                 </label>
                             </div>
                             <div className="mb-1 form-check">
-                                <input className="form-check-input me-2" type="radio" name="flexRadioDefault" id="flexRadioDefault6" />
+                                <input className="form-check-input me-2" type="radio" name="flexRadioDefault" id="flexRadioDefault6" 
+                                value="like_count desc"
+                                checked={filter === "comment_count desc"}
+                                onChange={onChangeFilter}
+                                />
                                 <label className="form-check-label" for="flexRadioDefault6">
                                     By Comment
                                 </label>
                             </div>
                             <div className="mb-1 form-check">
-                                <input className="form-check-input me-2" type="radio" name="flexRadioDefault" id="flexRadioDefault7" />
+                                <input className="form-check-input me-2" type="radio" name="flexRadioDefault" id="flexRadioDefault7" 
+                                value="like_count desc"
+                                checked={filter === "follower_count desc"}
+                                onChange={onChangeFilter}
+                                />
                                 <label className="form-check-label" for="flexRadioDefault7">
                                     By Follower
                                 </label>
@@ -93,55 +147,91 @@ const ListThread = (props) => {
                         <div className="title d-flex justify-content-center mb-1">Category</div>
                         <div className="content">
                             <div className="mb-1 form-check">
-                                <input className="form-check-input me-2" type="radio" name="flexRadioDefaults" id="flexRadioDefault17" checked/>
+                                <input className="form-check-input me-2" type="radio" name="flexRadioDefaults" id="flexRadioDefault17" 
+                                value=""
+                                checked={category === ""}
+                                onChange={onChangeCategory}
+                                />
                                 <label className="form-check-label" for="flexRadioDefault17" >
                                     None
                                 </label>
                             </div>
                             <div className="mb-1 form-check">
-                                <input className="form-check-input me-2" type="radio" name="flexRadioDefaults" id="flexRadioDefault9" />
+                                <input className="form-check-input me-2" type="radio" name="flexRadioDefaults" id="flexRadioDefault9" 
+                                value="health"
+                                checked={category === "health"}
+                                onChange={onChangeCategory}
+                                />
                                 <label className="form-check-label" for="flexRadioDefault9">
                                     Health
                                 </label>
                             </div>
                             <div className="mb-1 form-check">
-                                <input className="form-check-input me-2" type="radio" name="flexRadioDefaults" id="flexRadioDefault10" />
+                                <input className="form-check-input me-2" type="radio" name="flexRadioDefaults" id="flexRadioDefault10" 
+                                value="science"
+                                checked={category === "science"}
+                                onChange={onChangeCategory}
+                                />
                                 <label className="form-check-label" for="flexRadioDefault10">
                                     Science
                                 </label>
                             </div>
                             <div className="mb-1 form-check">
-                                <input className="form-check-input me-2" type="radio" name="flexRadioDefaults" id="flexRadioDefault11" />
+                                <input className="form-check-input me-2" type="radio" name="flexRadioDefaults" id="flexRadioDefault11" 
+                                value="technology"
+                                checked={category === "technology"}
+                                onChange={onChangeCategory}
+                                />
                                 <label className="form-check-label" for="flexRadioDefault11">
                                     Technology
                                 </label>
                             </div>
                             <div className="mb-1 form-check">
-                                <input className="form-check-input me-2" type="radio" name="flexRadioDefaults" id="flexRadioDefault12" />
+                                <input className="form-check-input me-2" type="radio" name="flexRadioDefaults" id="flexRadioDefault12" 
+                                value="lifestyle"
+                                checked={category === "lifestyle"}
+                                onChange={onChangeCategory}
+                                />
                                 <label className="form-check-label" for="flexRadioDefault12">
                                     Lifestyle
                                 </label>
                             </div>
                             <div className="mb-1 form-check">
-                                <input className="form-check-input me-2" type="radio" name="flexRadioDefaults" id="flexRadioDefault13" />
+                                <input className="form-check-input me-2" type="radio" name="flexRadioDefaults" id="flexRadioDefault13" 
+                                value="game"
+                                checked={category === "game"}
+                                onChange={onChangeCategory}
+                                />
                                 <label className="form-check-label" for="flexRadioDefault13">
                                     Game
                                 </label>
                             </div>
                             <div className="mb-1 form-check">
-                                <input className="form-check-input me-2" type="radio" name="flexRadioDefaults" id="flexRadioDefault14" />
+                                <input className="form-check-input me-2" type="radio" name="flexRadioDefaults" id="flexRadioDefault14" 
+                                value="sport"
+                                checked={category === "sport"}
+                                onChange={onChangeCategory}
+                                />
                                 <label className="form-check-label" for="flexRadioDefault14">
                                     Sport
                                 </label>
                             </div>
                             <div className="mb-1 form-check">
-                                <input className="form-check-input me-2" type="radio" name="flexRadioDefaults" id="flexRadioDefault15" />
+                                <input className="form-check-input me-2" type="radio" name="flexRadioDefaults" id="flexRadioDefault15" 
+                                value="food"
+                                checked={category === "food"}
+                                onChange={onChangeCategory}
+                                />
                                 <label className="form-check-label" for="flexRadioDefault15">
                                     Food
                                 </label>
                             </div>
                             <div className="mb-1 form-check">
-                                <input className="form-check-input me-2" type="radio" name="flexRadioDefaults" id="flexRadioDefault16" />
+                                <input className="form-check-input me-2" type="radio" name="flexRadioDefaults" id="flexRadioDefault16" 
+                                value="music"
+                                checked={category === "music"}
+                                onChange={onChangeCategory}
+                                />
                                 <label className="form-check-label" for="flexRadioDefault16">
                                     Music
                                 </label>
@@ -149,6 +239,7 @@ const ListThread = (props) => {
                         </div>
                     </div>
                 </div>
+                { filter == "" && category == "" ? 
                 <div className="mainContent col-sm-7">
                     <div className="bag mb-4">
                         <div className="head d-flex justify-content-between align-items-end mb-1 px-2">
@@ -255,7 +346,43 @@ const ListThread = (props) => {
                             ))}
                         </div>
                     </div>
-                </div>
+                </div> : 
+                <div className="mainContent col-sm-7">
+                    <div className="bag mb-4">
+                        <div className="head d-flex justify-content-between align-items-end mb-1 px-2">
+                            <div className="title fs-5">Result </div>
+                        </div>
+                        <div class="card-group">
+                            {filteredThread?.data?.map((item, index) => (
+                                <div class="card bg-transparent p-2 border-0" key={index}>
+                                    <Link to={`/forum/${item.id}-${item.title.toLowerCase().replace(/\s/g, "-")}`} className="link">
+                                    <img src={item.img} alt="..." className="card-img-top adjust mb-1"/>
+                                    <div className="wrapText my-2 fs-6">
+                                        {item.title.length >= 68 ? item.title.slice(0, 68)+"...." : item.title}
+                                    </div>
+                                    </Link>
+
+                                    <div className="d-flex justify-content-between">
+                                        <div className="d-flex">
+                                            <div className="like d-flex align-items-center me-3">
+                                                <img src={like} alt="" />
+                                                {item.like_count}
+                                            </div>
+                                            <div className="comment d-flex align-items-center">
+                                                <img src={comment} alt="" />
+                                                {item.comment_count}
+                                            </div>
+                                        </div>
+                                        <div className="follow d-flex align-items-center">Follow</div>
+                                    </div>
+                                    <p class="card-text text-end">
+                                        <small class="text-muted">Last updated 3 mins ago</small>
+                                    </p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </div> }
                 <div className="rightContent col-sm-3">
                     <Leaderboard data={mockRank} />
                 </div>
