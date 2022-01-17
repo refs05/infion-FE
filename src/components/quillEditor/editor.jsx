c2b76c6f8906ed4d73a331d6f2c4cee7dc9fb523
Restu Fajar Sidhiq <restufajarsidhiq02@gmail.com>
2022-01-17 16:27:20 +0700

    Back Editor Page

--- src/components/quillEditor/editor.jsx
+++ src/components/quillEditor/editor.jsx
@@ -1,27 +1,27 @@
-// import React from "react";
-// import ReactQuill from "react-quill";
-// import EditorToolbar, { modules, formats } from "./editorToolbar";
-// import "react-quill/dist/quill.snow.css";
-// import "./styles.css";
+import React from "react";
+import ReactQuill from "react-quill";
+import EditorToolbar, { modules, formats } from "./editorToolbar";
+import "react-quill/dist/quill.snow.css";
+import "./styles.css";
 
-// export const Editor = () => {
-//   const [state, setState] = React.useState({ value: null });
-//   const handleChange = (value) => {
-//     setState({ value });
-//   };
-//   return (
-//     <div className="text-editor">
-//       <EditorToolbar />
-//       <ReactQuill
-//         theme="snow"
-//         value={state.value}
-//         onChange={handleChange}
-//         placeholder={"Write something awesome..."}
-//         modules={modules}
-//         formats={formats}
-//       />
-//     </div>
-//   );
-// };
+export const Editor = () => {
+  const [state, setState] = React.useState({ value: null });
+  const handleChange = (value) => {
+    setState({ value });
+  };
+  return (
+    <div className="text-editor">
+      <EditorToolbar />
+      <ReactQuill
+        theme="snow"
+        value={state.value}
+        onChange={handleChange}
+        placeholder={"Write something awesome..."}
+        modules={modules}
+        formats={formats}
+      />
+    </div>
+  );
+};
 
-// export default Editor;
+export default Editor;
